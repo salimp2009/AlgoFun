6e56f8a1f0fee5e2cafaf1d70eff9b2d49c0db2b
salimp2009 <46407603+salimp2009@users.noreply.github.com>
2022-01-11 15:51:14 +0300

    tested the first passed implementation of the find_missing element; value has to have a default valuw of 1; the last pointer passed in std::partition is last + 1 since std::partition last is the last element +1 ; half is revised as ; unsigned half =(*last - *first+1) / 2 ; tested for 2 cases even and odd number vectors with one gap; will test for 2 gaps; or move on to iterator version

--- Algorithms/src/AlgoFun/AlgoFindMissingElem.hpp
+++ Algorithms/src/AlgoFun/AlgoFindMissingElem.hpp
@@ -9,17 +9,18 @@
 
 namespace algofun
 {
+    // write tests
     // changed unsigned* to Iterator, make the function generic and non-recursive; iterative
     // use concepts for the iterator type; Forward iterator and use STL iterator helper function
     // instead of pointer arithmetic
 
-     auto find_missing_element(unsigned* first, unsigned* last, unsigned value =0)
+     auto find_missing_element(unsigned* first, unsigned* last, unsigned value=1)
     {
-        if(first == last)
+        if(last==first)
         {
             return value;
         }
-        unsigned half =(last - first +1 ) / 2;
+        unsigned half =(*last - *first+1) / 2 ;
         unsigned m = half + value;
         auto p = std::partition(first, last, [&](auto elem) { return elem < m;} );
         if( p == first + half)
--- Algorithms/src/AlgoFun/AlgoFun.cpp
+++ Algorithms/src/AlgoFun/AlgoFun.cpp
@@ -13,6 +13,15 @@ namespace algofun
     void AlgoFun_Test()
     {
         std::puts("--AlgoFun_Test--");
+        std::vector<unsigned> vecOdd = {1,2,3,5,6,7,8};
+        std::vector<unsigned> vecEven = {1,2,3,4,6,7};
+        std::vector vecTwoGaps = {1,2,3,5,6,7,9,10,1,13};
+        std::printf("vecOdd end : %d \n", *(vecOdd.data()+vecOdd.size()-1));
+
+        auto valueOdd = find_missing_element(&vecOdd[0], &(vecOdd[vecOdd.size()-1]));
+        auto valueEven=find_missing_element(vecEven.data(), (vecEven.data()+vecEven.size()-1));
+        printf("vecOdd Missing Elem (expected: [4]: %d \n", valueOdd);
+        printf("vecEven Missing Elem (expected: [5]: %d \n", valueEven);
 
 
     }
