3cf2df93ece89a89754e6ef7b1c85cc601442bb0
salimp2009 <46407603+salimp2009@users.noreply.github.com>
2022-01-12 22:56:05 +0300

    find_missing_element; made the function constexpr and added const to variable inside the while loop, value bug is solved; actually it was not a bug if the range does not contain 0 then it should return zero as the missing element unless the start value is specified as the function parameter added a static_cast to return type std::distance since it returns signed integer added more compiler warnings into CMake; warning should a separate .cmake file and should be called from CMakeLists.txt added a doxygen document style /// @param so it shows in the Quick view of the function

--- Algorithms/src/AlgoFun/AlgoFindMissingElem.hpp
+++ Algorithms/src/AlgoFun/AlgoFindMissingElem.hpp
@@ -10,28 +10,33 @@
 namespace algofun
 {
 
-    // use STL iterator helper function; advance, next, distance...
-    // instead of pointer arithmetics
+    // linear time; n+n/2 +n/4..
+    // in place
+    // although forward_iterator is used it also supports random access, bi-directional iterators as well
+    // e.g: vectors, arrays, lists, forward iterators will work
 
-    ///@ A:find_missing_element()
-    ///@ linear time; n+n/2 +n/4..
-    ///@ in place
-    ///@ although forward_iterator is used it also supports random access, bi-directional iterators as well
-    ///@ e.g: vectors, arrays, lists, forward iterators will work
+    /* Contracts; pre-post conditions;
+     * (Pre) [fist,las) does not contain all possible value (there are missing elems)
+     * (Pre) last is reachable from first; using iterator helper functions
+     * (Pre?) [first, last) no duplicate values of T; not sure if this is needed!!
+     * (Pre?) value should not overflow if T is signed
+     */
+
+    /// @param  first   start iterator of a range/container
+    /// @param  last    end of the iterator (it is one past the last value)
+    /// @param  value   the start value of range; if not passed then default zero will be used
     template<std::forward_iterator It, typename T=std::iter_value_t<It>>
-     auto find_missing_element(It first, It last, T value=T{})
+     constexpr auto find_missing_element(It first, It last, T value=T{})
     {
       // added this instead of passing 1 as default to the value it works for numerical values
       // need to test for other types; if T{} is given then the search does not work
       // need to find a better way without this ???
-      ++value;
-
       while(last!=first)
       {
-          T half =(std::distance(first,last)+1 )/ 2 ;
-          T m = half + value;
-          auto p = std::partition(first, last, [&](auto elem) { return elem < m;} );
-          if( p == std::next(first, half))
+          const auto half =static_cast<T>(std::distance(first,last)+1)/ 2 ;
+          const T m = half + value;
+          const auto p = std::partition(first, last, [&](auto elem) { return elem < m;} );
+          if ( p == std::next(first, half))
           {
              first = p;
              value = m;
--- Algorithms/src/AlgoFun/AlgoFun.cpp
+++ Algorithms/src/AlgoFun/AlgoFun.cpp
@@ -13,8 +13,8 @@ namespace algofun
     void AlgoFun_Test()
     {
         std::puts("--AlgoFun_Test--");
-        std::vector<unsigned> vecOdd = {3,1,2,5,7,8,6};
-        std::vector<unsigned> vecEven = {1,2,3,4,6,7};
+        std::vector<unsigned> vecOdd = {0,3,1,2,5,7,8,6};
+        std::vector<unsigned> vecEven = {0,1,2,3,4,6,7};
         std::vector<unsigned >vecTwoGaps = {3,2,1,5,7,9,6,10,1,13};
         std::printf("vecOdd end : %d \n", *(vecOdd.data()+vecOdd.size()-1));
 
@@ -23,7 +23,7 @@ namespace algofun
         printf("vecOdd Missing Elem (expected: [4]): %d \n", valueOdd);
         printf("vecEven Missing Elem (expected: [5]): %d \n", valueEven);
 
-        auto valueTwoGaps = find_missing_element(vecTwoGaps.data(), (vecTwoGaps.data()+vecTwoGaps.size() ));
+        auto valueTwoGaps = find_missing_element(vecTwoGaps.data(), (vecTwoGaps.data()+vecTwoGaps.size()),1u);
         printf("vecTwoGaps Missing Elem (expected: [4 or 8]): %d \n", valueTwoGaps);
     }
 
