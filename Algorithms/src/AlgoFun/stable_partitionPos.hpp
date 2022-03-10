07bd3b162d8ed8bbede5d530f861209d1819fee5
salimp2009 <46407603+salimp2009@users.noreply.github.com>
2022-03-10 23:29:12 +0300

    uncommented the stable_partitionPos.hpp additions- a hack from GCC; for learning purposes of stable_partition and slightly modifiy to be able pass a buffer

--- Algorithms/src/AlgoFun/stable_partitionPos.hpp
+++ Algorithms/src/AlgoFun/stable_partitionPos.hpp
@@ -30,120 +30,119 @@ namespace algofun::details
 namespace algofun
 {
 
-//    template<typename InputIterator, typename Predicate, typename Distance>
-//    constexpr InputIterator
-//    find_if_not_nSP(InputIterator first, Distance& len, Predicate pred)
-//    {
-//        for (; len; --len,  (void) ++first)
-//            if (!__pred(first))
-//                break;
-//        return first;
-//    }
-//
-//    template<typename ForwardIterator, typename Pointer, typename Predicate,
-//             typename Distance>
-//    ForwardIterator
-//    stable_partition_adaptiveSP(ForwardIterator first,
-//                                ForwardIterator last,
-//                                Predicate pred, Distance len,
-//                                Pointer buffer,
-//                                Distance bufferSize)
-//    {
-//        if (len == 1)
-//            return first;
-//
-//        if (len <= bufferSize)
-//        {
-//            ForwardIterator result1 = first;
-//            Pointer result2 = buffer;
-//
-//            // The precondition guarantees that !pred(first), so
-//            // move that element to the buffer before starting the loop.
-//            // This ensures that we only call pred once per element.
-//            *result2 = std::move(*first);
-//            ++result2;
-//            ++first;
-//            for (; first != last; ++first)
-//                if (pred(first))
-//                {
-//                    *result1 = std::move(*first);
-//                    ++result1;
-//                }
-//                else
-//                {
-//                    *result2 = std::move(*first);
-//                    ++result2;
-//                }
-//
-//            std::move(buffer, result2, result1);
-//            return result1;
-//        }
-//
-//        ForwardIterator middle = first;
-//        std::advance(middle, len / 2);
-//        ForwardIterator leftSplit =
-//                stable_partition_adaptiveSP(first, middle, pred,
-//                                                 len / 2, buffer,
-//                                                 bufferSize);
-//
-//        // Advance past true-predicate values to satisfy this
-//        // function's preconditions.
-//        Distance rightLen = len - len / 2;
-//        ForwardIterator rightSplit =
-//
-//                find_if_not_nSP(middle, rightLen, pred);
-//
-//        if (rightLen)
-//            rightSplit =
-//                    std::__stable_partition_adaptive(rightSplit, last, pred,
-//                                                     rightLen,
-//                                                     buffer, bufferSize);
-//
-//        return std::rotate(leftSplit, middle, rightSplit);
-//    }
-//
-//    template<typename ForwardIterator, typename Predicate>
-//    ForwardIterator
-//    stable_partitionSP(ForwardIterator first, ForwardIterator last,
-//                       Predicate pred)
-//    {
-//        first = std::ranges::find_if_not(first, last, pred);
-//
-//        if (first == last)
-//            return first;
-//        std::stable_partition()
-//
+    template<typename InputIterator, typename Predicate, typename Distance>
+    constexpr InputIterator
+    find_if_not_nSP(InputIterator first, Distance& len, Predicate pred)
+    {
+        for (; len; --len,  (void) ++first)
+            if (!__pred(first))
+                break;
+        return first;
+    }
+
+    template<typename ForwardIterator, typename Pointer, typename Predicate,
+             typename Distance>
+    ForwardIterator
+    stablePartitionAdaptiveSp(ForwardIterator first,
+                                ForwardIterator last,
+                                Predicate pred, Distance len,
+                                Pointer buffer,
+                                Distance bufferSize)
+    {
+        if (len == 1)
+            return first;
+
+        if (len <= bufferSize)
+        {
+            ForwardIterator result1 = first;
+            Pointer result2 = buffer;
+
+            // The precondition guarantees that !pred(first), so
+            // move that element to the buffer before starting the loop.
+            // This ensures that we only call pred once per element.
+            *result2 = std::move(*first);
+            ++result2;
+            ++first;
+            for (; first != last; ++first)
+                if (pred(first))
+                {
+                    *result1 = std::move(*first);
+                    ++result1;
+                }
+                else
+                {
+                    *result2 = std::move(*first);
+                    ++result2;
+                }
+
+            std::move(buffer, result2, result1);
+            return result1;
+        }
+
+        ForwardIterator middle = first;
+        std::advance(middle, len / 2);
+        ForwardIterator leftSplit =
+                stablePartitionAdaptiveSp(first, middle, pred,
+                                          len / 2, buffer,
+                                          bufferSize);
+
+        // Advance past true-predicate values to satisfy this
+        // function's preconditions.
+        Distance rightLen = len - len / 2;
+        ForwardIterator rightSplit =
+
+                find_if_not_nSP(middle, rightLen, pred);
+
+        if (rightLen)
+            rightSplit =
+                    std::__stable_partition_adaptive(rightSplit, last, pred,
+                                                     rightLen,
+                                                     buffer, bufferSize);
+
+        return std::rotate(leftSplit, middle, rightSplit);
+    }
+
+    template<typename ForwardIterator, typename Predicate>
+    ForwardIterator
+    stablePartitionSp(ForwardIterator first, ForwardIterator last,
+                       Predicate pred)
+    {
+        first = std::ranges::find_if_not(first, last, pred);
+
+        if (first == last)
+            return first;
+
 //        typedef typename std::iterator_traits<ForwardIterator>::value_type
 //                value_type;
-//        typedef typename std::iterator_traits<ForwardIterator>::difference_type
-//                distanceType;
-//
+        typedef typename std::iterator_traits<ForwardIterator>::difference_type
+                distanceType;
+
 //        _Temporary_buffer<ForwardIterator, value_type>
 //                buf(first, std::distance(first, last));
-//        return
-//                stable_partition_adaptiveSP(first, last, pred,
-//                                           distanceType(buf.requested_size()),
-//                                           buf.begin(),
-//                                           distanceType(buf.size()));
-//    }
+        std::array<std::iter_reference_t<ForwardIterator>, std::distance(first, last)> buf;
+        return stablePartitionAdaptiveSp(first, last, pred,
+                                         distanceType(buf.requested_size()),
+                                         buf.begin(),
+                                         distanceType(buf.size()));
+    }
 
 
 
-//    struct stable_partition_SP
-//    {
-//            template<std::bidirectional_iterator It, std::sentinel_for<It> Sent,
-//                     typename Proj = std::identity,
-//                     std::indirect_unary_predicate<std::projected<It, Proj>> Pred>
-//            requires std::permutable<It>
-//                    std::ranges::subrange<It>
-//            operator()(It first, Sent last,
-//                       Pred pred, Proj proj = {}) const
-//            {
-//                auto lasti = std::ranges::next(first, last);
-//                auto middle = stable_partitionSP(std::move(first), lasti,
-//                                                std::invoke(pred, proj));
-//                return {std::move(middle), std::move(lasti)};
-//            }
+    struct stable_partitionSP
+    {
+            template<std::bidirectional_iterator It, std::sentinel_for<It> Sent,
+                     typename Proj = std::identity,
+                     std::indirect_unary_predicate<std::projected<It, Proj>> Pred>
+            requires std::permutable<It>
+                    std::ranges::subrange<It>
+            operator()(It first, Sent last,
+                       Pred pred, Proj proj = {}) const
+            {
+                auto lasti = std::ranges::next(first, last);
+                auto middle = stablePartitionSp(std::move(first), lasti,
+                                                std::invoke(pred, proj));
+                return {std::move(middle), std::move(lasti)};
+            }
 
 //            template<std::ranges::bidirectional_range _Range, typename _Proj = std::identity,
 //                     std::indirect_unary_predicate<std::projected<std::ranges::iterator_t<_Range>, _Proj>>
@@ -155,7 +154,7 @@ namespace algofun
 //                return (*this)(std::ranges::begin(__r), std::ranges::end(__r),
 //                               std::move(__pred), std::move(__proj));
 //            }
- //   };
+    };
 
 //    inline constexpr stable_partition_SP stable_partition{};
 
